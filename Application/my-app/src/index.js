820cf9cbe60c92ce8d9f7319312e61e2acb7e55d
ajoginapally <116819608+ajoginapally@users.noreply.github.com>
2024-02-24 10:04:14 -0500

    Update index.js

--- Application/my-app/src/index.js
+++ Application/my-app/src/index.js
@@ -3,6 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {openDatabase} from 'react-native-sqlite-storage';
+
+var db = openDatabase( {
+  name: 'User DataBase',
+  location: 'default'
+});
+const insertData = async() => {
+  const query_insert = 'INSERT INTO users {name, mobileNO, password} VALUES {?, ?, ?}';
+  const params = ['Xyz',  '123456789', '123'];
+
+  try {
+    (await db).executeSql(query_insert, params);
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+const updateData = async() => {
+  const query_update = 'UPDATE users SET mobileNo = ? WHERE id = ?';
+  const params = ['9879879879', '1'];
+
+  try {
+    (await db).executeSql(query_update, params);
+
+  } catch (err) {
+    console.log('err', err);
+  }
+
+  try {
+    (await db).executeSql(query_insert, params);
+  } catch (err) {
+    console.log('err', err);
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
